3293e61a99c879b3192b47234762d6954dd349cd
Urata Daiki <7nohe@users.noreply.github.com>
2024-09-15 23:29:17 +0900

    add user login functionality

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,17 +1,43 @@
-import { Head } from '@inertiajs/react'
+import { Head, Link } from '@inertiajs/react'
 
-export default function Home(props: { version: number }) {
+export default function Home(props: { version: number; user?: { email: string } }) {
+  const { user } = props
   return (
     <>
       <Head title="Homepage" />
-
+      <header
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '1rem',
+          borderBottom: '1px solid',
+        }}
+      >
+        <h1>Homepage</h1>
+        <nav
+          style={{
+            display: 'flex',
+            gap: '1rem',
+          }}
+        >
+          <ul
+            style={{
+              display: 'flex',
+              gap: '1rem',
+              listStyle: 'none',
+              alignItems: 'center',
+            }}
+          >
+            <li>
+              <Link href="/logout" method="post" as="button">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <div className="container">
-        <div className="title">AdonisJS {props.version} x Inertia x React</div>
-
-        <span>
-          Learn more about AdonisJS and Inertia.js by visiting the{' '}
-          <a href="https://docs.adonisjs.com/guides/inertia">AdonisJS documentation</a>.
-        </span>
+        <div className="title">You are logged in as {user?.email}</div>
       </div>
     </>
   )
--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,12 @@
 |
 */
 
+import { middleware } from './kernel.js'
 import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('home', { version: 6 })
+router.on('/').renderInertia('home', { version: 6 }).use([middleware.auth()])
 
+const UsersSessionController = () => import('#controllers/users/session_controller')
+
+router.get('login', [UsersSessionController, 'create'])
+router.post('login', [UsersSessionController, 'store'])
+router.post('logout', [UsersSessionController, 'destroy']).use(middleware.auth())
